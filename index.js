84040932e5b0ef66fec3c99c28c4765990b614e0
aminebouaziz <bouaziz.amine.18@gmail.com>
2019-01-20 11:53:51 +0100

    second commit

--- index.js
+++ index.js
@@ -24,16 +24,15 @@ app.get('/api/courses/:id',(req,res)=>{
 });
 
 app.post('/api/courses',(req,res)=> {
-    // const schema ={
-    //     name: Joi.string().min(3).required()
-    // };
-    // const  result =Joi.validate(req.body,schema);
-    // console.log(result);
-    // if(!req.body.name || req.body.length < 3) {
-    //     //400 bad request
-    //     res.status(400).send('eroor request');
-    //     return;
-    // }
+    const schema ={
+        name: Joi.string().min(3).required()
+    };
+    const  result =Joi.validate(req.body,schema);
+    if(result.error) {
+        //400 bad request
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
        const course ={
         id:courses.length +1,
         name: req.body.name
