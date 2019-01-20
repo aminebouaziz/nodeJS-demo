df6e1f899ade71720fef5e9659d97111b91ab945
aminebouaziz <bouaziz.amine.18@gmail.com>
2019-01-20 12:34:34 +0100

    second commit

--- index.js
+++ index.js
@@ -15,13 +15,6 @@ app.get('/',(req,res)=>{
 app.get('/api/courses',(req,res)=>{
     res.send([courses]);
 });
-app.get('/api/courses/:id',(req,res)=>{
-    const course = courses.find(c =>
-   c.id === parseInt(req.params.id) 
-                                )
-   if(!course) res.status(404).send('the course not found');//404
-    res.send(course);
-});
 
 app.post('/api/courses',(req,res)=> {
     const schema ={
@@ -40,6 +33,44 @@ app.post('/api/courses',(req,res)=> {
     courses.push(course);
     res.send(course);
 });
+
+
+app.put('/api/courses/:id',(req,res)=>{
+    const course = courses.find(c =>
+        c.id === parseInt(req.params.id) 
+                                     )
+        if(!course) res.status(404).send('the course not found');//404
+         res.send(course);
+   
+    
+    //  validate
+    //  if invalid, return 400 - bad request
+    const schema ={
+        name: Joi.string().min(3).required()
+    };
+    const  result =Joi.validate(req.body,schema);
+    if(result.error) {
+        //400 bad request
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    //  update course
+    course.name = req.body.name;
+    //  return the update course 
+    res.send(course);
+    });
+    
+
+
+
+app.get('/api/courses/:id',(req,res)=>{
+    const course = courses.find(c =>
+   c.id === parseInt(req.params.id) 
+                                )
+   if(!course) res.status(404).send('the course not found');//404
+    res.send(course);
+});
+
 
 
 
